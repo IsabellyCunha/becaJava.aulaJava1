d20fc6626c6198112c702435d0baf696c8fd8c8f
Isabelly Oliveira Cunha <iolivecu@everis.com>
2020-10-21 13:03:19 -0300

    Estruturas de Laços de Repetição e Switch Case

--- src/javaAula1/Aula1.java
+++ src/javaAula1/Aula1.java
@@ -1,10 +1,60 @@
 package javaAula1;
 
+import java.util.Scanner;
+
 public class Aula1 {
 
 	public static void main(String[] args) {
-		System.out.print("Hello world!");
-
+		System.out.println("Hello world!");
+		int i = 0;
+		
+		while(i <= 10) {
+			System.out.println("O valor de i é: " + i);
+			i++;
+		}
+		
+		/*
+		for(int i = 0; i<=10;i++) {
+			System.out.println("O valor de i é: " + i);
+		}
+		
+		/*Scanner ler = new Scanner(System.in);
+		int i = 0;
+		
+		System.out.println("Escolher o valor de i: ");
+		i = ler.nextInt();
+		
+		switch(i) {
+		case 1:
+			System.out.println("O valor de i: " + i);
+		break;
+		
+		case 2:
+			System.out.println("O valor de i: " + i);
+		break;
+		
+		case 3:
+			System.out.println("O valor de i: " + i);
+		break;
+		
+		default:
+			System.out.println("Esse valor não é válido");
+		break;
+		}
+		/*
+		//atribuindo valor boolean false a variável valor
+		boolean valor=false;
+				
+		//iniciando a estrutura if
+		if(valor == true) {
+		//Se o valor for = true, exiba a mensagem
+		System.out.print("Essa lógica é verdadeira");
+		}
+		else {
+			//Se o valor for = false, exiba a mensagem
+			System.out.print("Essa lógica é falsa");
+		}
+		*/
 	}
 
 }
